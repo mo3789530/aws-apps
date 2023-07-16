982fc8f5ec69bf8d50b553ff278692e7c3976470
mo <mo3789530@gmail.com>
2023-07-16 15:19:13 +0900

    wip

---
+++ aws-secret-register/app.py
@@ -0,0 +1,34 @@
+import yaml
+import boto3
+
+def config_open(filename: str) -> dict:
+    try:
+        with open(filename) as f:
+            return yaml.safe_load(f)
+    except Exception as e:
+        print(e)
+        print('Error opening config file')
+        exit(-1)
+
+def exist_ssm():
+    pass
+
+def register_ssm():
+    pass
+
+def exist_secretsmanager():
+    pass
+
+def register_secretsmanager():
+    pass
+
+def resource_name_format(app: dict):
+    pass
+
+def main():
+    yaml = config_open('application.yaml')
+    print(yaml.get('app'))
+
+
+if __name__ == '__main__':
+    main()
