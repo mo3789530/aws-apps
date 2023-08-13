0194c88d6b2cab964c2f7d82bca4d911c4e2b921
mo <mo3789530@gmail.com>
2023-08-13 14:14:46 +0900

    wip

--- aws-secret-register/app.py
+++ aws-secret-register/app.py
@@ -1,8 +1,22 @@
 import yaml
 import boto3
+from botocore.config import Config
+import os
+import subprocess
 
-def ssm():
-    pass
+
+def get_client(key, env):
+    config = Config(
+        region_name=env
+    )
+    return boto3.client(
+        key,
+        config=config,
+        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+        aws_session_token=os.environ.get("AWS_SESSION_TOKEN")
+    )
+
 
 def config_open(filename: str) -> dict:
     try:
@@ -13,20 +27,40 @@ def config_open(filename: str) -> dict:
         print('Error opening config file')
         exit(-1)
 
+
 def is_exist_ssm():
     pass
+
 
 def register_ssm():
     pass
 
-def is_exist_secretsmanager():
-    pass
 
-def register_secretsmanager():
-    pass
+def is_exist_secretsmanager(id: str):
+    client = client('secretsmanager')
+    try:
+        response = client.get_secret_value(SecretId=id)
+        return True
+    except:
+        return False
+
+
+def register_secretsmanager(name: str, secret: dict, tags: []):
+    client = get_client('secretsmanager')
+    try:
+        client.create_secret(
+            name=name,
+            SecretString=str(secret),
+            Tags=tags
+        )
+        print("success to register to secret manager")
+    except:
+        print("error")
+
 
 def resource_name_format(app: dict):
     pass
+
 
 def main():
     yaml = config_open('application.yaml')
