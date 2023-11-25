c3d594005a5c662bf9f392f31a88189453e7ff0c
mo <mo3789530@gmail.com>
2023-11-25 14:18:49 +0900

    wip

--- aws-secret-register/app.py
+++ aws-secret-register/app.py
@@ -4,13 +4,10 @@ from botocore.config import Config
 import os
 
 
-def get_client(key, env):
-    config = Config(
-        region_name=env
-    )
+def get_client(key: str, region: str = "ap-northeast-1"):
     return boto3.client(
         key,
-        config=config,
+        region_name=region,
         aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
         aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
         aws_session_token=os.environ.get("AWS_SESSION_TOKEN")
@@ -27,19 +24,20 @@ def config_open(filename: str) -> dict:
         exit(-1)
 
 
-def is_exist_ssm(name: str) -> bool:
-    client = get_client('ssm')
+def is_exist_ssm(name: str, app: dict) -> bool:
+    client = get_client('ssm', app.get("region"))
     try:
         # Retrieve the parameter value
         response = client.get_parameter(Name=name, WithDecryption=True)
         parameter_value = response['Parameter']['Value']
         return True
     except client.exceptions.ParameterNotFound:
-        print(f"Parameter '{name}' not found.")
         return False
+    except Exception as e:
+        print(e)
 
 
-def register_ssm(name: str, value: str | [], description: str, parameter_type: str, tags: []):
+def register_ssm(name: str, value: str | list, description: str, parameter_type: str, tags: [], app: dict):
     client = get_client('ssm')
     try:
         response = client.put_parameter(
@@ -53,24 +51,25 @@ def register_ssm(name: str, value: str | [], description: str, parameter_type: s
     except Exception as e:
         print(e)
 
-def is_exist_secretsmanager(id: str) -> bool:
-    client = get_client('secretsmanager')
+
+def is_exist_secretsmanager(id: str, app: dict = {}) -> bool:
+    client = get_client('secretsmanager', app.get("region"))
     try:
         response = client.get_secret_value(SecretId=id)
         return True
-    except:
+    except client.exceptions.ResourceNotFoundException:
         return False
 
 
 def tags(app) -> []:
     return [
-        {"SID": app.sid},
-        {"Environment": app.env}
+        {"SID": app.get("sid")},
+        {"Environment": app.get("env")}
     ]
 
 
-def register_secretsmanager(name: str, secret: dict, tags: []):
-    client = get_client('secretsmanager')
+def register_secretsmanager(app: dict, name: str, secret: dict, tags: []):
+    client = get_client('secretsmanager', app.get("resgion"))
     try:
         client.create_secret(
             name=name,
@@ -78,11 +77,13 @@ def register_secretsmanager(name: str, secret: dict, tags: []):
             Tags=tags
         )
         print("success to register to secret manager")
-    except:
-        print("error")
+    except Exception as e:
+        print("error: " + e)
 
 
-def resource_name_format(app: dict, name: str) -> str:
+def resource_name_format(app: dict, name: str, is_ssm: bool) -> str:
+    if is_ssm:
+        return f'/{app.get("env")}-{app.get("sid")}-{name}'
     return f'{app.get("env")}-{app.get("sid")}-{name}'
 
 
@@ -90,7 +91,13 @@ def main():
     yaml = config_open('application.yaml')
     print(yaml.get('app'))
     app = yaml.get('app')
-    print(resource_name_format(app, "test"))
+    print(tags(app=app))
+    print(resource_name_format(app, "test", True))
+    print(is_exist_secretsmanager("test"))
+    print(is_exist_ssm("test", app=app))
+    secret = yaml.get("secret")
+    for k in secret.keys():
+        print(secret[k])
 
 
 if __name__ == '__main__':
--- aws-secret-register/test_app.py
+++ aws-secret-register/test_app.py
@@ -1,8 +1,19 @@
 import unittest
 import app
 
+
 class TestApp(unittest.TestCase):
     def test_tags(self):
         yaml = {'sid': 'test', 'env': 'dev', 'region': 'ap-northeast-1'}
         tags = app.tags(yaml)
-        self.assertEqual()
+        _except = [{'SID': 'test'}, {'Environment': 'dev'}]
+        self.assertEqual(_except, tags)
+
+    def test_resource_name(self):
+        yaml = {'sid': 'test', 'env': 'dev', 'region': 'ap-northeast-1'}
+        _except = "/dev-test-test"
+        name = app.resource_name_format(yaml, "test", True)
+        self.assertEqual(_except, name)
+        _except = "dev-test-test"
+        name = app.resource_name_format(yaml, "test", False)
+        self.assertEqual(_except, name)
