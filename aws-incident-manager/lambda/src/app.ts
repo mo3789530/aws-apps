060ed37e6c66888da9ba01c0e2a49951bda1ead3
mo3789530 <mo3789530@gmail.com>
2024-01-20 23:54:15 +0900

    add 404 error if page is not found

--- aws-incident-manager/lambda/src/app.ts
+++ aws-incident-manager/lambda/src/app.ts
@@ -1,63 +1,34 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import Helmet from 'helmet'
 
 export const app = express();
 
+
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: "*" ,
+  preflightContinue: false,
+};
+
+app.use(cors(options));
+app.use(Helmet());
+
 app.use(bodyParser.json());
 
-type Person = {
-  name: string;
-  age: number;
-};
-
-const users: { [id: string]: Person } = {
-  sazae: {
-    name: 'フグ田サザエ',
-    age: 24,
-  },
-  hamihei: {
-    name: '磯野海平',
-    age: 54,
-  },
-  fune: {
-    name: '磯野フネ',
-    age: 50,
-  },
-  masuo: {
-    name: 'フグ田マスオ',
-    age: 28,
-  },
-  katsuo: {
-    name: '磯野カツオ',
-    age: 11,
-  },
-  wakame: {
-    name: '磯野ワカメ',
-    age: 9,
-  },
-  tarao: {
-    name: 'フグ田タラオ',
-    age: 3,
-  },
-};
-
-app.get('/echo', (req: Request, res: Response) => {
-  const message = req.query['message'];
-
-  res.send(`Hello ${message}!!`);
+app.get('/api/health', (req: Request, res: Response) => {
+  res.status(200).send({status: 'ok'});
 });
 
-app.post('/echo', (req: Request, res: Response) => {
-  const message = req.body['message'];
-
-  res.send({ message: `Hello ${message}!!` });
-});
-
-app.get('/users', (req: Request, res: Response) => {
-  res.send(Object.values(users));
-});
-
-app.get('/users/:id', (req: Request, res: Response) => {
-  const id = req.params['id'];
-  res.send(users[id]);
-});
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).send("Not Found");
+})
